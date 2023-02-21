f48b2777618de2613848f54d26b79a8f4383f65f
Kaladin17 <dekazanov8@gmail.com>
2023-02-21 17:41:30 +0600

    last commit

---
+++ app/src/main/java/com/kazanov/kazanov_2m_5_3/MainActivity.java
@@ -0,0 +1,32 @@
+package com.kazanov.kazanov_2m_5_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button button = findViewById(R.id.button);
+        EditText editText = findViewById(R.id.username);
+        TextView textView = findViewById(R.id.welcome);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = editText.getText().toString();
+                textView.setText("Welcome " + text);
+
+            }
+        });
+    }
+}
